034196cce3240bf4f1e7e47e98a65cdad23f54d6
Sarojv04 <sarojv0407@gmail.com>
2026-04-25 00:10:21 -0400

    commit

--- Easy/181. Employees Earning More Than Their Managers.sql
+++ Easy/181. Employees Earning More Than Their Managers.sql
@@ -5,3 +5,11 @@ from Employee as e
 join Employee as m
 on e.managerId = m.id
 where e.salary  > m.salary;
+
+-- second solution
+
+select e.name as Employee
+from Employee as e
+left join Employee as m
+on  e.managerId = m.id
+where e.salary > m.salary;
