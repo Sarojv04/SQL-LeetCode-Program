4e21130a446116a87c2e7bd3eec1cf0edc17a38e
Sarojv04 <sarojv0407@gmail.com>
2026-04-27 23:27:32 -0400

    solution

--- Easy/196. Delete Duplicate Emails.sql
+++ Easy/196. Delete Duplicate Emails.sql
@@ -1,19 +1,21 @@
 -- 196. Delete Duplicate Emails
 
-delete p1
-from Person as p1
-join Person as p2
-on p1.email = p2.email
-and p1.id > p2.id;
+-- delete p1
+-- from Person as p1
+-- join Person as p2
+-- on p1.email = p2.email
+-- and p1.id > p2.id;
 
--- 2nd way
+-- -- 2nd way
 
-delete from Person
-where id not in (
-    select id from
-    (
-    select min(id) as id
-    from Person
-    group by email
-    ) as temp
-);
+-- delete from Person
+-- where id not in (
+--     select id from
+--     (
+--     select min(id) as id
+--     from Person
+--     group by email
+--     ) as temp
+-- );
+
+
