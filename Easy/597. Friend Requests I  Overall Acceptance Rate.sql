732d56e23bc1e88a15256325fed6d82407b72ebc
Sarojv04 <sarojv0407@gmail.com>
2026-05-02 19:51:15 -0400

    Commit

---
+++ Easy/597. Friend Requests I  Overall Acceptance Rate.sql
@@ -0,0 +1,12 @@
+-- 597. Friend Requests I: Overall Acceptance Rate
+
+
+select (
+        round(
+            ifnull(
+                (select count(distinct requester_id, accepter_id) from RequestAccepted)
+                /
+                (select count(distinct sender_id,send_to_id)  from FriendRequest)  
+            ,0)
+            ,2)   
+) as accept_rate;
