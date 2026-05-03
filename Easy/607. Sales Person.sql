135da11d8c068a75558bdea938dc74a861b71091
Sarojv04 <sarojv0407@gmail.com>
2026-05-02 21:31:40 -0400

    Commit

---
+++ Easy/607. Sales Person.sql
@@ -0,0 +1,11 @@
+-- 607. Sales Person
+
+select name 
+from SalesPerson 
+where sales_id not in (
+                           select o.sales_id 
+                           from Orders as o
+                           left join Company as c
+                           on o. com_id = c.com_id 
+                           where c.name = 'RED'
+                       );
