d55874bfe59a4ade5d254ad435e88472eb6e5322
Sarojv04 <sarojv0407@gmail.com>
2026-05-19 22:01:15 -0400

    commit

--- Easy/183. Customers Who Never Order.sql
+++ Easy/183. Customers Who Never Order.sql
@@ -6,8 +6,10 @@ left join Orders as o
 on c.id = o.customerId 
 where o.customerId is NULL;
 
--- Second way to solve the que
+-- Second way 
 
 select c.name as Customers
 from  Customers as c
 where Id not in (select customerId from Orders);
+
+-- finished
