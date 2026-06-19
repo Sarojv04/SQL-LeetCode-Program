c1f43d93beaa6ef392398a46aac8ca47c76d11fe
Sarojv04 <sarojv0407@gmail.com>
2026-06-19 03:50:28 -0400

    commit

---
+++ Easy/1083. Sales Analysis II.sql
@@ -0,0 +1,9 @@
+select s.buyer_id 
+from Sales as s
+left join Product as p
+on s.product_id = p.product_id 
+where p.product_name = 'S8' and s.buyer_id  not in (select s.buyer_id  
+from Sales as s
+left join Product  as p
+on s.product_id = p.product_id 
+where p.product_name = 'iPhone');
