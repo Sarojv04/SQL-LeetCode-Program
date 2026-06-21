4ecf3994dae7d3c2130f8394acb24d5b09db1583
Sarojv04 <sarojv0407@gmail.com>
2026-06-21 02:36:29 -0400

    commit

--- Easy/1173. Immediate Food Delivery I.sql
+++ Easy/1173. Immediate Food Delivery I.sql
@@ -2,3 +2,10 @@ select round(temp * 100/(select count(delivery_id)from Delivery),2) as immediate
 from (select count(delivery_id)as temp
 from Delivery 
 where order_date = customer_pref_delivery_date)t;
+
+-- second method
+
+select round(
+    100 * sum(order_date = customer_pref_delivery_date)/count(delivery_id),2
+) as immediate_percentage
+from Delivery;
