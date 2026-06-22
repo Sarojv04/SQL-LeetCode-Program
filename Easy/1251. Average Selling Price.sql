e5d250637e1488cc6739d56b19fcf9937dba64bc
Sarojv04 <sarojv0407@gmail.com>
2026-06-22 11:58:25 -0400

    commit

---
+++ Easy/1251. Average Selling Price.sql
@@ -0,0 +1,5 @@
+select p.product_id, round(sum(u.units * p.price)/sum(u.units),2) as average_price
+from Prices as p
+left join UnitsSold as u
+on p.product_id = u.product_id and purchase_date  between start_date and end_date 
+group by product_id;  
